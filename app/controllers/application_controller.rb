1746719f10cb0c20f91b99baf8d6ebd6f0cd3bed
Nikolay Turnaviotov <foxmuldercp@users.noreply.github.com>
2016-04-01 22:38:30 +0000

    Fix flash to twitter bootstrap alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :sanitize_params, if: :devise_controller?
+#  before_action :sanitize_params, if: :devise_controller?
 end
